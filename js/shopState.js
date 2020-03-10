695553d4ae6b3f68df9ba528787491d02c112120
Yannick Félix <yannick.felix1999@gmail.com>
2020-03-10 21:24:12 +0100

    Changed Status values 0~5 to -1~6

--- js/shopState.js
+++ js/shopState.js
@@ -5,6 +5,8 @@ function orderInfo(hash) {
     let state2 = $("#state2");
     let state3 = $("#state3");
     let state4 = $("#state4");
+    let state5 = $("#state5");
+    let state6 = $("#state6");
     let stateN = $("#stateN");
 
     state0.addClass("grey-text bolden");
@@ -12,12 +14,17 @@ function orderInfo(hash) {
     state2.addClass("grey-text bolden");
     state3.addClass("grey-text bolden");
     state4.addClass("grey-text bolden");
+    state5.addClass("grey-text bolden");
+    state6.addClass("grey-text bolden");
     state0.removeClass("green-text");
     state1.removeClass("green-text");
     state2.removeClass("green-text");
     state3.removeClass("green-text");
     state4.removeClass("green-text");
+    state5.removeClass("green-text");
+    state6.removeClass("green-text");
     stateN.hide();
+    state5.hide();
 
     $("#shopHome").hide();
     $.getJSON("backend/api/order/checkInfo.php",{orderID: orderID}, (json) => {
@@ -31,6 +38,16 @@ function orderInfo(hash) {
         $("#stateInfoAmou").html(json.items.length-1+" Positionen");
 
         switch(json.state) {
+            case 6:
+                state6.removeClass("grey-text");
+                state5.removeClass("bolden");
+                state6.addClass("green-text");
+            case 5:
+                state5.removeClass("grey-text");
+                state4.removeClass("bolden");
+                state4.hide();
+                state5.show();
+                state5.addClass("green-text");
             case 4:
                 state4.removeClass("grey-text");
                 state3.removeClass("bolden");
@@ -51,7 +68,7 @@ function orderInfo(hash) {
                 state0.removeClass("grey-text");
                 state0.addClass("green-text");
                 break;
-            case 5:
+            case -1:
                 stateN.show();
                 state0.removeClass("bolden");
                 state0.removeClass("grey-text");
